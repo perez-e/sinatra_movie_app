b701d57425bf22fe38515c244595472338c6280a
Eric Perez <ericricardoperez@gmail.com>
2014-01-16 16:36:45 -0800

    added post request to /result route

--- movie_app.rb
+++ movie_app.rb
@@ -8,3 +8,11 @@ get "/" do
 
 	erb :index
 end
+
+
+post "/result" do 
+	search_str = params[:movie]
+
+	response = Typhoeus.get("http://www.omdbapi.com/", params: {s: search_str})
+	result = JSON.parse(response.body)
+end
